8a39f6676dbcdb57fa283fece43561aa6966d1d6
Sacha Lewin <sacha.lewin@me.com>
2022-12-06 18:09:02 +0100

    json etc

---
+++ json_writer.py
@@ -0,0 +1,35 @@
+import json
+
+class JsonWriter(object):
+    def __init__(self, path):
+        self.path = path
+        self.file = open(path + '.json', 'w')
+        self.data = []
+
+    def write_frame(self, frame_id, boxes):
+        nb_boxes = boxes.shape[0]
+        for i in range(nb_boxes):
+            box = boxes[i]
+            # x1, y1, x2, y2, score, class
+            x1 = box[0].item()
+            y1 = box[1].item()
+            x2 = box[2].item()
+            y2 = box[3].item()
+            score = box[4].item()
+            cat_id = 2 if box[5].item() == 0 else 1
+            if cat_id == 1:  # ball
+                depth = 0 # TODO
+                self.write_ball(frame_id, x1, y1, x2, y2, depth, score)
+            elif cat_id == 2:  # person
+                self.write_person(frame_id, x1, y1, x2, y2, score)
+
+    def write_ball(self, id, x1, y1, x2, y2, z, score):
+        data = {'image_id': id, 'bbox': [x1, y1, x2 - x1, y2 - y1], 'category_id': 1, 'score': score, 'distance': z}
+        self.data.append(data)
+
+    def write_person(self, id, x1, y1, x2, y2, score):
+        data = {'image_id': id, 'bbox': [x1, y1, x2 - x1, y2 - y1], 'category_id': 2, 'score': score}
+        self.data.append(data)
+        
+    def close(self):
+        json.dump(self.data, self.file, indent=4)
--- main.py
+++ main.py
@@ -2,18 +2,24 @@ import cv2
 import torch
 from motion_detection import MotionDetector
 from argparse import ArgumentParser
+from feed_reader import VideoFeedReader
+from json_writer import JsonWriter
+import time
 
 # Parse arguments
 parser = ArgumentParser()
 # Mode: offline (parse images in a folder) or online (read camera stream)
 parser.add_argument('-o', '--online', type=lambda x: (str(x).lower() == 'true'), default=True, help='offline or online')
 # Path to the folder containing the images (offline mode only)
-parser.add_argument('-p', '--path', type=str, default='images/ref_5_1_', help='Path prefix of the sequence images')
+parser.add_argument('-p', '--path', type=str, default='data/img_5_1/img_5_1_', help='Path prefix of the sequence images')
 # Produce a video with the angle overlayed on the frames
 # Online mode shows a live feed, offline mode saves the video.
-parser.add_argument('-v', '--video', type=lambda x: (str(x).lower() == 'true'), default=True, help='produce a video with the angle overlayed on the frames')
+parser.add_argument('-sv', '--savevideo', type=lambda x: (str(x).lower() == 'true'), default=False, 
+                    help='produce a video with the angle overlayed on the frames')
+parser.add_argument('-dv', '--displayvideo', type=lambda x: (str(x).lower() == 'true'), default=True, 
+                    help='display a video with the angle overlayed on the frames')
 # Frames between two updates of the angle estimation
-parser.add_argument('-s', '--skip', type=int, default=15, help='frames between two updates of the angle estimation')
+parser.add_argument('-s', '--skip', type=int, default=1, help='frames between two updates of the angle estimation')
 # Print the angle in the console
 parser.add_argument('-d', '--print', type=lambda x: (str(x).lower() == 'true'), default=False, help='print the angle in the console')
 
@@ -21,15 +27,18 @@ parser.add_argument('-d', '--print', type=lambda x: (str(x).lower() == 'true'), 
 args = parser.parse_args()
 online = args.online
 path = args.path
-video = args.video
+save_video = args.savevideo
+display_video = args.displayvideo
 frames_per_update = args.skip
 print_angle = args.print
+save_bbox = True
+
 
 # Calibrate camera, no matter the parameters.
-cam_matrix = calibrate_camera('calibration', debug=True)
+#cam_matrix = calibrate_camera('calibration', debug=True)
 
 # set parameters for text drawn on the frames
-font = cv.FONT_HERSHEY_COMPLEX
+font = cv2.FONT_HERSHEY_COMPLEX
 fontScale = 2
 fontColor = (255, 0, 0)
 lineType  = 3
@@ -42,15 +51,20 @@ motion_type = 'None'
 
 curr_angle = 0
 frame_count = 1
+frame_id = 0
 
-if video and not online:
+if save_video:
     # initialise video writer
     file_name = "results/" + path.split('/')[-1] + '.mp4'
-    codec = cv.VideoWriter_fourcc(*'mp4v')
-    outputStream = cv.VideoWriter(file_name, codec, 25.0, (1280, 720), 0)
-
+    codec = cv2.VideoWriter_fourcc(*'mp4v')
+    outputStream = cv2.VideoWriter(file_name, codec, 25.0, (1280, 720), 0)
 
 vfr = VideoFeedReader(online=online, path=path)
+md = MotionDetector(model='yolov5n')
+if save_bbox:
+    jw = JsonWriter('json/' + path.split('/')[-1])
+
+start_time = time.time()
 previous_frame = vfr.read()
 while True:
     # Read frame
@@ -59,9 +73,16 @@ while True:
     # If the frame is empty, break immediately
     if current_frame is None:
         break
+    
+    res = md.detect(current_frame)
+    boxes = res['boxes']
+
+    if save_bbox:
+        jw.write_frame(frame_id, boxes)
 
     if frame_count == frames_per_update:
-        curr_angle += find_angle(previous_frame, current_frame, cam_matrix)
+        #curr_angle += find_angle(previous_frame, current_frame, cam_matrix)
+        curr_angle += 0
         if print_angle:
             print("Angle: {}".format(curr_angle))
 
@@ -69,24 +90,39 @@ while True:
     
         # Update the previous frame
         previous_frame = current_frame.copy()
-    frame_count += 1
     
-    if video:
-        cv.putText(current_frame, str(curr_angle), (50,90), 
+    if display_video:
+        for box in boxes:
+            txt = 'person' if box[5] == 0 else 'ball'
+            color = (0, 0, 255) if box[5] == 0 else (255, 0, 0)
+            cv2.putText(current_frame, txt, (box[0], box[1] + 15), font, 0.8, color, lineType)
+            cv2.rectangle(current_frame, (box[0], box[1]), (box[2], box[3]), color, 2)
+
+        cv2.putText(current_frame, str(curr_angle), (50,90), 
                     font, fontScale, fontColor, lineType)
         # Display the resulting frame
-        cv.imshow('Frame', current_frame)
-        if cv.waitKey(1) & 0xFF == ord('q'):
+        cv2.imshow('Frame', current_frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
             break
-        if not online:
-            outputStream.write(current_frame)
+    
+    if save_video:
+        outputStream.write(current_frame)
 
-if video and not online:
+    frame_count += 1
+    frame_id += 1
+
+if save_video:
     print('Video saved to {}'.format(file_name))
     outputStream.release()
+
+if save_bbox:
+    jw.close()
+
+time_elapsed = time.time() - start_time
+print("Finished in {} seconds (avg. FPS: {})".format(time_elapsed, frame_id / time_elapsed))
 
 # After the loop release the cap object
 vfr.release()
 
 # Destroy all the windows
-cv.destroyAllWindows()
+cv2.destroyAllWindows()
