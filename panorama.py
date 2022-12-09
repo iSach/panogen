b817e0d183d96c4a7039833c459cae749cd000ec
LionelLrc <lionel.larocca@student.uliege.be>
2022-12-09 18:24:14 +0100

    add panorama.py

---
+++ panorama.py
@@ -0,0 +1,216 @@
+# https://github.com/KEDIARAHUL135/PanoramaStitchingP2
+import cv2
+import numpy as np
+from anglemeter import __get_panning_angle
+
+MIN_MATCH_COUNT = 10
+
+# https://docs.opencv.org/3.4/d1/de0/tutorial_py_feature_homography.html
+def FindMatches(BaseImage, SecImage):
+    img1 = cv2.cvtColor(BaseImage, cv2.COLOR_BGR2GRAY) # queryImage
+    img2 = cv2.cvtColor(SecImage, cv2.COLOR_BGR2GRAY) # trainImage
+
+    # Initiate SIFT detector
+    sift = cv2.SIFT_create()
+
+    # find the keypoints and descriptors with ORB
+    kp1, des1 = sift.detectAndCompute(img1, None)
+    kp2, des2 = sift.detectAndCompute(img2, None)
+
+    # BFMatcher with default params
+    FLANN_INDEX_KDTREE = 1
+    index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
+    search_params = dict(checks = 50)
+    flann = cv2.FlannBasedMatcher(index_params, search_params)
+    matches = flann.knnMatch(des1,des2,k=2) # knnMatch is crucial
+
+    # Apply ratio test
+    good = []
+    for m, n in matches: # for every descriptor, take closest two matches
+        if m.distance < 0.7 * n.distance: # best match has to be this much closer than second best
+            good.append(m)
+
+    return good, kp1, kp2
+
+# https://docs.opencv.org/3.4/d1/de0/tutorial_py_feature_homography.html
+def FindHomography(good, kp1, kp2, mask1):
+    if len(good)>MIN_MATCH_COUNT:
+        src_pts = np.float32([ kp1[m.queryIdx].pt for m in good ]).reshape(-1,1,2)
+        dst_pts = np.float32([ kp2[m.trainIdx].pt for m in good ]).reshape(-1,1,2)
+        M, mask = cv2.findHomography(dst_pts, src_pts, cv2.RANSAC,5.0, mask=mask1)
+        matchesMask = mask.ravel().tolist()
+    else:
+        print( "Not enough matches are found - {}/{}".format(len(good), MIN_MATCH_COUNT) )
+        matchesMask = None
+        print("\nNot enough matches found between the images.\n")
+        exit(0)
+
+    return M, matchesMask
+
+
+def GetNewFrameSizeAndMatrix(HomographyMatrix, Sec_ImageShape, Base_ImageShape):
+    # Reading the size of the image
+    (Height, Width) = Sec_ImageShape
+    
+    # Taking the matrix of initial coordinates of the corners of the secondary image
+    # Stored in the following format: [[x1, x2, x3, x4], [y1, y2, y3, y4], [1, 1, 1, 1]]
+    # Where (xt, yt) is the coordinate of the i th corner of the image. 
+    InitialMatrix = np.array([[0, Width - 1, Width - 1, 0],
+                            [0, 0, Height - 1, Height - 1],
+                            [1, 1, 1, 1]])
+    
+    # Finding the final coordinates of the corners of the image after transformation.
+    # NOTE: Here, the coordinates of the corners of the frame may go out of the 
+    # frame(negative values). We will correct this afterwards by updating the 
+    # homography matrix accordingly.
+    FinalMatrix = np.dot(HomographyMatrix, InitialMatrix)
+
+    [x, y, c] = FinalMatrix
+    x = np.divide(x, c)
+    y = np.divide(y, c)
+
+    # Finding the dimentions of the stitched image frame and the "Correction" factor
+    min_x, max_x = int(round(min(x))), int(round(max(x)))
+    min_y, max_y = int(round(min(y))), int(round(max(y)))
+
+    New_Width = max_x
+    New_Height = max_y
+    Correction = [0, 0]
+    if min_x < 0:
+        New_Width -= min_x
+        Correction[0] = abs(min_x)
+    if min_y < 0:
+        New_Height -= min_y
+        Correction[1] = abs(min_y)
+    
+    # Again correcting New_Width and New_Height
+    # Helpful when secondary image is overlaped on the left hand side of the Base image.
+    if New_Width < Base_ImageShape[1] + Correction[0]:
+        New_Width = Base_ImageShape[1] + Correction[0]
+    if New_Height < Base_ImageShape[0] + Correction[1]:
+        New_Height = Base_ImageShape[0] + Correction[1]
+
+    # Finding the coordinates of the corners of the image if they all were within the frame.
+    x = np.add(x, Correction[0])
+    y = np.add(y, Correction[1])
+    OldInitialPoints = np.float32([[0, 0],
+                                [Width - 1, 0],
+                                [Width - 1, Height - 1],
+                                [0, Height - 1]])
+    NewFinalPonts = np.float32(np.array([x, y]).transpose())
+
+    # Updating the homography matrix. Done so that now the secondary image completely 
+    # lies inside the frame
+    HomographyMatrix = cv2.getPerspectiveTransform(OldInitialPoints, NewFinalPonts)
+    
+    return [New_Height, New_Width], Correction, HomographyMatrix
+
+
+def StitchImages(BaseImage, SecImage, camera_matrix, mask):
+    """
+    Create a panorama 
+    Args:
+        BaseImage (np.array): first frame.
+        SecImage (np.array): second frame.
+        camera_matrix (np.array): Calibrated camera matrix.
+        mask: an 8-bit single channel image that specifies the regions of the input image that should be included in the computation
+    Returns:
+        image: panorama
+        float: panning angle in degrees.
+    """
+    # Applying Cylindrical projection on SecImage
+    SecImage_Cyl, mask_x, mask_y = ProjectOntoCylinder(SecImage)
+
+    # Getting SecImage Mask
+    SecImage_Mask = np.zeros(SecImage_Cyl.shape, dtype=np.uint8)
+    SecImage_Mask[mask_y, mask_x, :] = 255
+
+    # Finding matches between the 2 images and their keypoints
+    Matches, BaseImage_kp, SecImage_kp = FindMatches(BaseImage, SecImage_Cyl)
+    
+    # Finding homography matrix.
+    HomographyMatrix, Status = FindHomography(Matches, BaseImage_kp, SecImage_kp, mask)
+    
+    # Finding size of new frame of stitched images and updating the homography matrix 
+    NewFrameSize, Correction, HomographyMatrix = GetNewFrameSizeAndMatrix(HomographyMatrix, SecImage_Cyl.shape[:2], BaseImage.shape[:2])
+
+    # Finally placing the images upon one another.
+    SecImage_Transformed = cv2.warpPerspective(SecImage_Cyl, HomographyMatrix, (NewFrameSize[1], NewFrameSize[0]))
+    SecImage_Transformed_Mask = cv2.warpPerspective(SecImage_Mask, HomographyMatrix, (NewFrameSize[1], NewFrameSize[0]))
+    BaseImage_Transformed = np.zeros((NewFrameSize[0], NewFrameSize[1], 3), dtype=np.uint8)
+    BaseImage_Transformed[Correction[1]:Correction[1]+BaseImage.shape[0], Correction[0]:Correction[0]+BaseImage.shape[1]] = BaseImage
+
+    StitchedImage = cv2.bitwise_or(SecImage_Transformed, cv2.bitwise_and(BaseImage_Transformed, cv2.bitwise_not(SecImage_Transformed_Mask)))
+
+    _, rotations, _, _ = cv2.decomposeHomographyMat(HomographyMatrix, K=camera_matrix)
+
+    return StitchedImage, __get_panning_angle(rotations[0])
+
+
+def Convert_xy(x, y):
+    global center, f
+
+    xt = ( f * np.tan( (x - center[0]) / f ) ) + center[0]
+    yt = ( (y - center[1]) / np.cos( (x - center[0]) / f ) ) + center[1]
+    
+    return xt, yt
+
+
+def ProjectOntoCylinder(InitialImage, cam_matrix):
+    global w, h, center, f
+    h, w = InitialImage.shape[:2]
+    center = [w // 2, h // 2]
+    f = cam_matrix[0,0]
+    
+    # Creating a blank transformed image
+    TransformedImage = np.zeros(InitialImage.shape, dtype=np.uint8)
+    
+    # Storing all coordinates of the transformed image in 2 arrays (x and y coordinates)
+    AllCoordinates_of_ti =  np.array([np.array([i, j]) for i in range(w) for j in range(h)])
+    ti_x = AllCoordinates_of_ti[:, 0]
+    ti_y = AllCoordinates_of_ti[:, 1]
+    
+    # Finding corresponding coordinates of the transformed image in the initial image
+    ii_x, ii_y = Convert_xy(ti_x, ti_y)
+
+    # Rounding off the coordinate values to get exact pixel values (top-left corner)
+    ii_tl_x = ii_x.astype(int)
+    ii_tl_y = ii_y.astype(int)
+
+    # Finding transformed image points whose corresponding 
+    # initial image points lies inside the initial image
+    GoodIndices = (ii_tl_x >= 0) * (ii_tl_x <= (w-2)) * \
+                (ii_tl_y >= 0) * (ii_tl_y <= (h-2))
+
+    # Removing all the outside points from everywhere
+    ti_x = ti_x[GoodIndices]
+    ti_y = ti_y[GoodIndices]
+    
+    ii_x = ii_x[GoodIndices]
+    ii_y = ii_y[GoodIndices]
+
+    ii_tl_x = ii_tl_x[GoodIndices]
+    ii_tl_y = ii_tl_y[GoodIndices]
+
+    # Bilinear interpolation
+    dx = ii_x - ii_tl_x
+    dy = ii_y - ii_tl_y
+
+    weight_tl = (1.0 - dx) * (1.0 - dy)
+    weight_tr = (dx)       * (1.0 - dy)
+    weight_bl = (1.0 - dx) * (dy)
+    weight_br = (dx)       * (dy)
+    
+    TransformedImage[ti_y, ti_x, :] = ( weight_tl[:, None] * InitialImage[ii_tl_y,     ii_tl_x,     :] ) + \
+                                    ( weight_tr[:, None] * InitialImage[ii_tl_y,     ii_tl_x + 1, :] ) + \
+                                    ( weight_bl[:, None] * InitialImage[ii_tl_y + 1, ii_tl_x,     :] ) + \
+                                    ( weight_br[:, None] * InitialImage[ii_tl_y + 1, ii_tl_x + 1, :] )
+
+
+    # Getting x coorinate to remove black region from right and left in the transformed image
+    min_x = min(ti_x)
+
+    # Cropping out the black region from both sides (using symmetricity)
+    TransformedImage = TransformedImage[:, min_x : -min_x, :]
+
+    return TransformedImage, ti_x-min_x, ti_y
